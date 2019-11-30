aa0705ea85bfcaf98171f1761639bc633c400b52
adrisanc <adrian_sc@live.com>
2019-11-30 10:30:11 +0100

    Refactor to coding standards

--- src/Controller/ResetRolesController.php
+++ src/Controller/ResetRolesController.php
@@ -1,32 +1,27 @@
 <?php
-/**
-* @file
-* Contains \Drupal\reset_roles\Controller\ResetRolesController.
-*/
+
 namespace Drupal\reset_roles\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\user\Entity\Role;
-
 
 /**
-* Controller for return the content of the pages request 
-*/
+ * Controller for return the content of the pages request .
+ */
 class ResetRolesController extends ControllerBase {
-	
-	/*public function list() {		
-		$rolesMultiples = Role::loadMultiple();
-		$roles = [];		
-		foreach ($rolesMultiples as $role) {			
-			$roles[]= [
-				'label' => $role->label(),
-				'id' => $role->id()
-			];
-		}
-		
-		return array(			
-			'#theme' => 'list-roles',
-			'#items' => $roles,			
-		);
-	}*/
+
+  /*public function list() {
+  $rolesMultiples = Role::loadMultiple();
+  $roles = [];
+  foreach ($rolesMultiples as $role) {
+  $roles[]= [
+  'label' => $role->label(),
+  'id' => $role->id()
+  ];
+  }
+
+  return array(
+  '#theme' => 'list-roles',
+  '#items' => $roles,
+  );
+  }*/
 }
--- src/Form/RolesForm.php
+++ src/Form/RolesForm.php
@@ -2,126 +2,153 @@
 
 namespace Drupal\reset_roles\Form;
 
+use Drupal\Component\Utility\Random;
+use Drupal\user\Entity\User;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\user\Entity\Role;
 
-
+/**
+ *
+ */
 class RolesForm extends FormBase {
 
-	
-	public function __construct() {
-		$roles = [];
-		$rolesMultiples;
-		$usersToReset;
-	}
+  /**
+   *
+   */
+  public function __construct() {
+    $roles = [];
+    $rolesMultiples;
+    $usersToReset;
+  }
 
-	public function getFormId() {
+  /**
+   *
+   */
+  public function getFormId() {
 
-		return 'reset_roles_form';
+    return 'reset_roles_form';
 
-	}
+  }
 
-	public function buildForm(array $form, FormStateInterface $form_state) {
-		
+  /**
+   *
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
 
-		$this->rolesMultiples = Role::loadMultiple();
+    $this->rolesMultiples = Role::loadMultiple();
 
-		foreach ($this->rolesMultiples as $role) {			
-			if ($role->id() != 'anonymous') {
-				$form[$role->id()] = [
-					'#type' => 'checkbox',
-					'#title' => $role->label(),
-				];	
-			}			
-		}
-			
-		$form['submit'] = [
-			'#type' => 'submit',
-			'#value'=> $this->t('Reset pasword by role'),
-		];
+    foreach ($this->rolesMultiples as $role) {
+      if ($role->id() != 'anonymous') {
+        $form[$role->id()] = [
+          '#type' => 'checkbox',
+          '#title' => $role->label(),
+        ];
+      }
+    }
 
-		return $form;
-	}
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Reset pasword by role'),
+    ];
 
-	public function submitForm(array &$form, FormStateInterface $form_state) {
-		
-		$checkedRolesObjects = array_filter(array_filter($form, function($e) {
-			return $e['#type']=='checkbox';
-		}),function($e) {
-			return $e['#checked']==1;
-		});		
+    return $form;
+  }
 
-		$checkedRolesTitles = array_map(function($value) { 
-			return $value['#title'];
-		}, $checkedRolesObjects);
+  /**
+   *
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
 
-		//Get machine name of roles
-		$checkedRolesNames = array_map(function($value) { 
-			return $value['#name'];
-		}, $checkedRolesObjects);
+    $checkedRolesObjects = array_filter(array_filter($form, function ($e) {
+      return $e['#type'] == 'checkbox';
+    }), function ($e) {
+      return $e['#checked'] == 1;
+    });
 
-		
-		//Get list of uids of users 
-		$query = \Drupal::entityQuery('user');
-		$uids = $query->execute();
-	
-		$this->usersToReset = $this->checkUsers($uids, $checkedRolesNames);
-		
-		$this->resetPasswordOfUsers($this->usersToReset);
-		drupal_set_message(t('Se han encontrado un total de ' . sizeof($this->usersToReset) . ' usuarios.'), 'status');
-		
-	}
+    $checkedRolesTitles = array_map(function ($value) {
+      return $value['#title'];
+    }, $checkedRolesObjects);
 
+    // Get machine name of roles.
+    $checkedRolesNames = array_map(function ($value) {
+      return $value['#name'];
+    }, $checkedRolesObjects);
 
+    // Get list of uids of users.
+    $query = \Drupal::entityQuery('user');
+    $uids = $query->execute();
 
-	public function checkUsers($uids, $roles) {
-		
-		$users;
-		$usersGlobals = array_map(function($uid){			
-			return \Drupal\user\Entity\User::load($uid);				
-		}, $uids);		
+    $this->usersToReset = $this->checkUsers($uids, $checkedRolesNames);
 
+    $this->resetPasswordOfUsers($this->usersToReset);
+    drupal_set_message(t('Se han encontrado un total de ' . sizeof($this->usersToReset) . ' usuarios.'), 'status');
 
-		foreach ($usersGlobals as $key => $user) {			
-			foreach ($roles as $key => $role) {
-				if(in_array($role, $user->getRoles())) {
-					$users[] = $user;
-				}
-			}
-		}
+  }
 
-		return $users;
-		
-	}
+  /**
+   *
+   */
+  public function checkUsers($uids, $roles) {
 
-	public function resetPasswordOfUsers($users) {
-		$random = new \Drupal\Component\Utility\Random();
-		$string = $random->string();		
-		array_map(function($uid){			
-		    $mailManager = \Drupal::service('plugin.manager.mail');
-		    $langcode = \Drupal::currentUser()->getPreferredLangcode();
-		    $params['context']['subject'] = "Reset password of " . \Drupal::config('system.site')->get('name');
-		    $params['context']['message'] = "This is a simply email to reset password. Next you have a url to reset password of site: <br> " . user_pass_reset_url($uid) . "";
-		    $to = $uid->getEmail();
-		    $secure_check = $this->sanitize_my_email($to);
-			if ($secure_check != false) {
-				$user_storage = \Drupal::entityManager()->getStorage('user');
-				$user = $user_storage->load($uid->id());
-				$uid->setPassword($string);
-				$uid->save();
-			    $mailManager->mail('system', 'mail', $to, $langcode, $params);
-			}
-		}, $users);
-	}
+    $users;
+    $usersGlobals = array_map(function ($uid) {
+      return User::load($uid);
+    }, $uids);
 
-	function sanitize_my_email($field) {
-	    $field = filter_var($field, FILTER_SANITIZE_EMAIL);
-	    if (filter_var($field, FILTER_VALIDATE_EMAIL)) {
-	        return true;
-	    } else {
-	        return false;
-	    }
-	 }
+    foreach ($usersGlobals as $key => $user) {
+      foreach ($roles as $key => $role) {
+        if (in_array($role, $user->getRoles())) {
+          $users[] = $user;
+        }
+      }
+    }
+
+    return $users;
+
+  }
+
+  /**
+   *
+   */
+  public function resetPasswordOfUsers($users) {
+    $random = new Random();
+    array_map(function ($uid) use ($random) {
+            $mailManager = \Drupal::service('plugin.manager.mail');
+            $langcode = \Drupal::currentUser()->getPreferredLangcode();
+            $params['context']['subject'] = "Reset password of " . \Drupal::config('system.site')->get('name');
+            $params['context']['message'] = "This is a simply email to reset password. Next you have a url to reset password of site: <br> " . user_pass_reset_url($uid) . "";
+            $to = $uid->getEmail();
+            $secure_check = $this->sanitize_my_email($to);
+      if ($secure_check != FALSE) {
+        $user_storage = \Drupal::entityManager()->getStorage('user');
+        $user = $user_storage->load($uid->id());
+        // Reset password of user.
+        $string = $random->string();
+        $uid->setPassword($string);
+        $uid->save();
+        // Logout user inmediatly.
+        \Drupal::currentUser()->setAccount($uid);
+        if (\Drupal::currentUser()->isAuthenticated()) {
+          $session_manager = \Drupal::service('session_manager');
+          $session_manager->delete(\Drupal::currentUser()->id());
+        }
+        $mailManager->mail('system', 'mail', $to, $langcode, $params);
+      }
+    }, $users);
+  }
+
+  /**
+   *
+   */
+  public function sanitize_my_email($field) {
+    $field = filter_var($field, FILTER_SANITIZE_EMAIL);
+    if (filter_var($field, FILTER_VALIDATE_EMAIL)) {
+      return TRUE;
+    }
+    else {
+      return FALSE;
+    }
+  }
+
 }
-	
